bf3ee41587e6268c00eb6e304c26de1f3e802eda
Alvin See <alvin.see@me.com>
2014-08-11 00:53:43 -0700

    some login styling

--- sql_test/login.php
+++ sql_test/login.php
@@ -1,27 +1,41 @@
 <html>
+<head>
+    <title>login</title>
+    <meta charset="UTF-8">
+      <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300italic,600%7CSource+Code+Pro" rel="stylesheet" />
+      <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="login.css">
+</head>
 <body>
 <?php
-    if(isset($_POST['login']))
-            {
-                        $username = $_POST['username'];
-                                $password = $_POST['password'];
-                                $con = mysqli_connect('localhost','root','Tw0sof+9Ly','sample');
-                                        $result = mysqli_query($con, "SELECT * FROM `users` WHERE username='$username' AND password='$password'");
-                                        if(mysqli_num_rows($result) == 0)
-                                                        echo 'Invalid username or password';
-                                                else
-                                                                echo '<h1>Logged in</h1><p>A Secret for you....</p>';
-                                            }
+    if(isset($_POST['login'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $con = mysqli_connect('localhost','root','Tw0sof+9Ly','sample');
+        $result = mysqli_query($con, "SELECT * FROM `users` WHERE username='$username' AND password='$password'");
+        if(mysqli_num_rows($result) == 0)
+            echo 'Invalid username or password';
         else
-                {
+            echo '<h1>Logged in</h1><p>A Secret for you....</p>';
+    }
+    else {
 ?>
-        <form action="" method="post">
-            Username: <input type="text" name="username"/><br />
-            Password: <input type="password" name="password"/><br />
-            <input type="submit" name="login" value="Login"/>
-        </form>
+<section id="loginBox">
+    <h2>login</h2>
+    <form method="post" class="minimal">
+        <label for="username">
+            <input type="text" name="username" id="username" placeholder="username" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{4,20}$" required="required" />
+        </label>
+        <label for="password">
+            <input type="password" name="password" id="password" placeholder="password"  required="required" />
+        </label>
+        <button type="submit" class="btn-minimal" name="login">sign in</button>
+    </form>
+</section>
 <?php
-                        }
+    }
 ?>
 </body>
 </html>
+
+</body> 
