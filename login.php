e6f4b74067ffa135c5ee15f74237d9830abaa15f
Alvin See <alvin.see@me.com>
2014-08-11 05:39:34 -0700

    moved login.css

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
       <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300italic,600%7CSource+Code+Pro" rel="stylesheet" />
       <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
       <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
-<link rel="stylesheet" type="text/css" href="/login.css">
+      <link rel="stylesheet" href="/css/login.css">
 </head>
 <body>
 <?php
