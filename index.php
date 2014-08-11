5331e28f39b57cf99c553cec890158ca2446b6a1
Alvin See <alvin.see@me.com>
2014-08-11 02:39:04 -0700

    please

--- index.php
+++ index.php
@@ -83,11 +83,13 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
                </section>
                <section class="4u">
                   <div class="profile-circular"></div>
+                  <div>
                   <h4>Alvin See</h4>
                   <p><i class="fa fa-github"></i> alvinsee</p>
                   <p><i class="fa fa-envelope-o"></i> aysee@ucsd.edu</p>
                   <br>
                   <p><i class="fa fa-user"></i> A computer ENGINEERING student who has a very nihilistic view on life. I like taking long walks on the beach and watching the sunset.</p>
+                  </div>
                </section>
                <section class="4u">
                   <h4>Th***s Po**ll</h4>
