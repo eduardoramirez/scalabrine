34d0d9b383d0aabf95f5e305a26ecd9347a7b679
Alvin See <alvin.see@me.com>
2014-08-19 12:13:59 -0700

    added ico to errordoc

--- 404.php
+++ 404.php
@@ -4,6 +4,8 @@
       <title>scalabrine | 404</title>
       <meta charset="utf-8">
       <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300italic,600%7CSource+Code+Pro" rel="stylesheet" />
+      <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+      <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
       <!--[if lte IE 8]><script src="js/html5shiv.js" type="text/javascript"></script><![endif]-->
       <script src="/js/skel.min.js"></script>
       <script>
