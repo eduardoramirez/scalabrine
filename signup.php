80c2af79a4e0001ba39afe901024167158f0f7d7
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-19 22:09:39 -0700

    signup page test

--- signup.php
+++ signup.php
@@ -40,7 +40,11 @@
           ];
 
           $h_password = password_hash($password, PASSWORD_BCRYPT, $options);
-          
+             $sql="INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$h_password')";
+
+            mysqli_query($con,$sql);
+            $_SESSION['signup'] = "";
+            header("Location: /index");         
           // Username is free
           if($res && mysql_num_rows($res) == 0) {
             $sql="INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$h_password')";
