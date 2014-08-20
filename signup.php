52c0120c0d68385799cd72b9ef79061bff4b1d33
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-19 21:58:23 -0700

    signup page test

--- signup.php
+++ signup.php
@@ -44,7 +44,7 @@
           // Username is free
           if($res && mysql_num_rows($res) == 0) {
             $sql="INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$h_password')";
-
+echo $sql;
             mysqli_query($con,$sql);
             $_SESSION['signup'] = "";
             header("Location: /index");
