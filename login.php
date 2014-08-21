5b1eba9e90ea64a7403a5630af28951db907b5ec
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-21 00:14:41 -0700

    updated login page test

--- login.php
+++ login.php
@@ -31,6 +31,7 @@
                 $_SESSION['login'] = "";
                 header("HTTP/1.1 403 Forbidden");
                 header("Location: /403");
+                exit();
             } 
 
             if(password_verify($password, $db_password))
