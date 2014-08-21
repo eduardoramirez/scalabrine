fd5576c6713b93d6e5899f40c02eff70f142f1f3
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-21 15:38:28 -0700

    testing perl

--- perl/WebApp/simple.pl
+++ perl/WebApp/simple.pl
@@ -3,6 +3,9 @@ use strict;
 use lib '/var/www/html/perl/WebApp/libs';
 use MyLib::Simple;
 
+print "content-type: text/html\n\n";
+print "hello world\n";
+
 my $webapp = MyLib::Simple->new(
   PARAMS => {
     cfg_file => ['simple.ini'],
