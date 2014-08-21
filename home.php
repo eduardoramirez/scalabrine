45a5bf841855004273b49eacef6dccc826f05e3e
Kenny Mai <kenmai9@gmail.com>
2014-08-21 13:28:41 -0700

    Update home.php
    updated to make it have columns

--- home.php
+++ home.php
@@ -48,35 +48,34 @@
          </section>
          <!-- Main -->
          <!-- things about our product should go here -->
-         <div class="container narrow">
-            <div class="row marketing">
-               <div class="col-lg-3">
+         <article>
+            <div class="row">
+               <section class="4u">
                   <h4>JavaScript Error Collecting</h4>
                   <p>We have a service endpoint that collects the errors sent in as JavaScript errors happen.</p>
-               </div>
-               <div class="col-lg-3">
+               </section>
+               <section class="4u">
                   <h4>Account Creation and Management</h4>
                   <p>Users who wish to track their errors on their website can create an account, which may have many users. Additionally, each account will have full user management (Add, Edit and Delete users including setting passworsd and access). Users may also recover their own password using our password recovery system.</p>
-               </div>
-               <div class="col-lg-3">
+               </section>
+               <section class="4u">
                   <h4>Dashboard Page for Recent Errors</h4>
                   <p>Users who create an account can see their recent errors in a grid and chart.</p>
-               </div>
-               <div class="col-lg-3">
+               </section>
+               <section class="4u">
                   <h4>Error Detail Page</h4>
                   <p>Users can see an error detail page that show an error incident and provides an area for other users to comment on the errors and rate severity of an error (1-5 stars). Users can also upload screenshots.</p>
-               </div>
-               <div class="col-lg-3">
+               </section>
+               <section class="4u">
                   <h4>Configuration Page</h4>
                   <p>This configuration page is for developers to get the script for the site they will track errors on.</p>
-               </div>
-               <div class="col-lg-3">
+               </section>
+               <section class="4u">
                   <h4>Admin Dashboard</h4>
                   <p>This admin dashboard allows the admin to see who logs in and logs out of the system, add/edit/delete clients, and get basic stats on overall usage and error rates across accounts.</p>
-               </div>
+               </section>
             </div>
-            
-         </div>
+         </article>
          <!-- Footer -->
          <footer>
             <div id="footer">
