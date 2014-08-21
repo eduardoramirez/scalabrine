45c295ee2cd17bfa3b0819bbe50f8ad075a8f613
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-20 20:10:41 -0700

    signup page test

--- signup.php
+++ signup.php
@@ -37,10 +37,10 @@
 
         if(strcmp($password, $confirm_password) === 0)
         {
-          $res = mysqli_query($con, "SELECT * FROM `users` WHERE username='$username'");
+          $res = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
 
           // Username is free
-          if(mysql_num_rows($res) === 0) {
+          if(mysql_num_rows($res) == 0) {
 
             $h_password = password_hash($password, PASSWORD_BCRYPT, $options);
             $sql="INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$h_password')";
