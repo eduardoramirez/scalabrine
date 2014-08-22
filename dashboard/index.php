c2b72570fa7867b24976be57f5abefad517d1627
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-22 16:31:47 -0700

    fixed redirecting skill

--- dashboard/index.php
+++ dashboard/index.php
@@ -103,7 +103,7 @@
                </li>
 
                <li>
-                  <a href="/index.html">
+                  <a href="/index.php">
                      <i class="icon-user"></i>
                      <span>home</span>
                   </a>
