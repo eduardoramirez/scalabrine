b7301cb156135f552c6b67c0ecbd014c42c1b047
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-22 20:34:57 -0700

    testing perl

--- perl/login.pl
+++ perl/login.pl
@@ -1,16 +1,10 @@
 #!/usr/bin/perl
 use CGI;
 use DBI;
-use Crypt::PBKDF2;
 use DateTime;
 
 # read the CGI params
 my $cgi = CGI->new;
-
-#$session = CGI::Session()->new(undef, $cgi, {Directory=>'/var/cgi-bin/'});
-#$CGISESSID = $session->id();
-
-#print $session->header();
 
 my $username = $cgi->param("username");
 my $password = $cgi->param("password");
@@ -33,7 +27,7 @@ my $time = DateTime->now;
 
 # create a JSON string according to the database result
 my $json = ($password eq "opensesame") ?
-  qq{{"success" : "login is successful", "userid" : "$username" "time":"$time"}} :
+  qq{{"success" : "login is successful", "userid" : "$username", "time" : "$time"}} :
   qq{{"error" : "username or password is wrong"}};
 
 # return JSON string
