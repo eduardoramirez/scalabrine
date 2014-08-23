b3ad41baf6901b453b45539402ac45a6112347d6
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-22 20:18:38 -0700

    testing perl

--- perl/login.js
+++ perl/login.js
@@ -5,11 +5,7 @@ $(document).ready(function(){
 
     $.ajax({
       type: "GET",
-<<<<<<< HEAD
-      url: "104.131.195.41:9093/cgi-bin/", // URL of the Perl script
-=======
       url: "/cgi-bin/login2.pl", // URL of the Perl script
->>>>>>> 3a7b625bfb916aaac1815d7c7ceb8852dfdfa7af
       contentType: "application/json; charset=utf-8",
       dataType: "json",
       // send username and password as parameters to the Perl script
@@ -17,21 +13,14 @@ $(document).ready(function(){
       // script call was *not* successful
       error: function(XMLHttpRequest, textStatus, errorThrown) { 
         // error occure in http request
-         alert("something went wrong!");
-
-          $('div#loginResult').text("responseText: " + XMLHttpRequest.responseText 
-            + ", textStatus: " + textStatus 
-            + ", errorThrown: " + errorThrown);
-          $('div#loginResult').addClass("error")
+                  alert("womps");
       }, // error 
       // script call was successful 
       // data contains the JSON values returned by the Perl script 
       success: function(data){
-        if (data.error){
-          alert("some error!");
-          $('div#loginResult').text("data.error: " + data.error);
-          $('div#loginResult').addClass("error");
-        // show what kind of error the user received (wrong username -- wrong pass)
+        if (data.error){          alert("didnt worked!");
+         // add this <div class="alert alert-danger" role="alert">incorrect password.</div>
+          //wrong password
         } // if
         else { // login was successful
           // move the user to the dashboard
@@ -40,7 +29,7 @@ $(document).ready(function(){
             $('div#loginResult').text("data.success: " + data.success 
               + ", data.userid: " + data.userid);
             $('div#loginResult').addClass("success");
-            location.href = "/index"
+            //location.href = "/index"
         } //else
       } // success
     }); // ajax
