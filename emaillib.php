4a258d60a3f5d7ab7c7d9a7b58dd8067cce02b2d
Eduardo Ramirez <eduardo@ucsd.edu>
2014-08-27 23:46:16 -0700

    clean up of unnecessary things

--- emaillib.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-define("SMTP_HOST", "smtp.gmail.com"); 
-define("SMTP_PORT", "465"); 
-define("SMTP_UNAME", "scalabrinecse@gmail.com"); 
-define("SMTP_PWORD", "313v3nsof+9Ly");
-?>
