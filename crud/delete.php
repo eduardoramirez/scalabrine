8950deb17ab7e2bccd60b1d30f2cb471e9e235cd
Matt Asaro <mattasaro@gmail.com>
2014-09-01 16:09:32 -0700

    fixed crud/delete.php role check

--- crud/delete.php
+++ crud/delete.php
@@ -1,46 +1,45 @@
 <?php
 session_start();
+
+$id = 0;
+if ( !empty($_GET['id'])) {
+    $id = $_REQUEST['id'];
+}
+
 if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
     header("Location: /dashboard/login");
 }
-else if ($_SESSION['admin'] == 0){
+else if ($_SESSION['admin'] == 0 || empty($_GET['id'])){
     header("HTTP/1.1 403 Forbidden");
     header("Location: /403");
 }
+else if ($_SESSION['admin'] == 1){
+    $orgID = $_SESSION['orgID'];
+
+    $sql = 'SELECT count(*) FROM user WHERE ID = ' . $id . ' and OrgID = ' . $_SESSION['orgID'];
+    $pdo = Database::connect();
+    $result = $pdo->query($sql);
+
+    if ($result == 0){
+        header("HTTP/1.1 403 Forbidden");
+        header("Location: /403");
+    }
+}
 else{
 	require 'database.php';
-	$id = 0;
-	
-	if ( !empty($_GET['id'])) {
-		$id = $_REQUEST['id'];
-	}
 
-    if ($_SESSION['admin'] == 1){
-        $orgID = $_SESSION['orgID'];
+    if ( !empty($_POST)) {
+        // keep track post values
+        $id = $_POST['id'];
 
-        $sql = 'SELECT count(*) FROM user WHERE ID = ' . $id . ' and OrgID = ' . $_SESSION['orgID'];
+        // delete data
         $pdo = Database::connect();
-        $result = $pdo->query($sql);
-
-        if ($result == 0){
-            header("HTTP/1.1 403 Forbidden");
-            header("Location: /403");
-        }
-        else{
-            if ( !empty($_POST)) {
-                // keep track post values
-                $id = $_POST['id'];
-
-                // delete data
-                $pdo = Database::connect();
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $sql = "DELETE FROM user WHERE ID = ?";
-                $q = $pdo->prepare($sql);
-                $q->execute(array($id));
-                Database::disconnect();
-                header("Location: index.php");
-            }
-        }
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM user WHERE ID = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        Database::disconnect();
+        header("Location: index.php");
     }
 ?>
 
