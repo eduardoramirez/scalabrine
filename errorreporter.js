a157db23d61926a236c84bfb6a1cb69bf883a1bf
Alvin See <alvin.see@me.com>
2014-09-01 13:37:25 -0700

    js error url

--- errorreporter.js
+++ errorreporter.js
@@ -3,7 +3,7 @@ var ErrorTracking = {};
 
 
 /* URL of your server-side error recording script */
-ErrorTracking.errorReportingURL = "104.131.195.41:9091/seterrors";
+ErrorTracking.errorReportingURL = "/seterrors";
 
 
 
