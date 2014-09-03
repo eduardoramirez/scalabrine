edc5c0ea17fc999850464b2772650b35bf244b83
Eduardo Ramirez <eduardo@ucsd.edu>
2014-09-02 23:58:09 -0700

    can modify fields without password change

--- crud/update.php
+++ crud/update.php
@@ -44,10 +44,6 @@
 			$valid = false;
 		}
 		
-		if (empty($password)) {
-			$passwordError = 'Please enter Password';
-			$valid = false;
-		}
 		
 //////////
     $con = mysqli_connect('localhost','root','Tw0sof+9Ly','scalabrinedb');
@@ -285,9 +281,6 @@
 					    <label class="control-label">Password</label>
 					    <div class="controls">
 					      	<input name="password" type="password"  placeholder="Password">
-                  <?php if (!empty($passwordError)): ?>
-                    <span class="help-inline"><?php echo $passwordError;?></span>
-                  <?php endif;?>
 					    </div>
                       </div>
                       <?php if($_SESSION['admin']==1): ?>
