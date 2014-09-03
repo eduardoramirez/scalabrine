31467e18098f134eb9d1b271957f46b7ec3d4a8c
Eduardo Ramirez <eduardo@ucsd.edu>
2014-09-03 11:15:15 -0700

    standardized db

--- crud/update.php
+++ crud/update.php
@@ -3,8 +3,8 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
   session_start();
-	require 'database.php';
 
+  require 'database.php';
 	$id = null;
 	if ( !empty($_GET['id'])) {
 		$id = $_REQUEST['id'];
