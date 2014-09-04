9c33b7f0c878100bb24b61bde538fc6f1c10142b
Matt Asaro <mattasaro@gmail.com>
2014-09-03 21:47:45 -0700

    fixed log out event for session log

--- dashboard/logout.php
+++ dashboard/logout.php
@@ -2,6 +2,7 @@
   session_start();
 
     if(isset($_SESSION['username'])){
+        $username = $_SESSION['username'];
         $data = my_query('s', array(&$username), "SELECT ID FROM user WHERE Username=?");
         $id = $data['ID'];
 
