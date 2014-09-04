d4e50cb853ceaa1b68d30375eb9ef0fd0ea952cc
vgadetsk <vgadetsk@ucsd.edu>
2014-09-04 03:51:32 -0700

    new 404.php

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
       <script src="/old/js/skel.min.js"></script>
       <script>
       skel.init({
-         prefix: '/old/css/style',
+         prefix: '/min/?f=old/css/style',
          preloadStyleSheets: true,
          resetCSS: true,
          boxModel: 'border',
