8a7ae9a6fc638af122917839543e8dee31c56192
Alvin See <alvin.see@me.com>
2014-09-03 22:07:04 -0700

    testing post comment

--- dashboard/error_detail.php
+++ dashboard/error_detail.php
@@ -280,9 +280,25 @@ $id_post = "1"; //the post or the page id
 
                               </div>
                               <div class="chat-form">
+                              <!--
                                   <div class="input-cont ">
                                       <input type="text" placeholder="Type a message here..." class="form-control col-lg-12">
                                   </div>
+                                  -->
+
+    <div class="new-com-bt">
+        <span>Write a comment ...</span>
+    </div>
+    <div class="new-com-cnt">
+        <input type="text" id="name-com" name="name-com" value="" placeholder="Your name" />
+        <input type="text" id="mail-com" name="mail-com" value="" placeholder="Your e-mail adress" />
+        <textarea class="the-new-com"></textarea>
+        <div class="bt-add-com">Post comment</div>
+        <div class="bt-cancel-com">Cancel</div>
+    </div>
+    <div class="clear"></div>
+
+
                                   <div class="form-group">
                                       <div class="pull-right chat-features">
                                           <a href="javascript:;">
@@ -323,6 +339,60 @@ $id_post = "1"; //the post or the page id
 <?php
 }
 ?>
+
+<script type="text/javascript">
+   $(function(){ 
+        //alert(event.timeStamp);
+        $('.new-com-bt').click(function(event){    
+            $(this).hide();
+            $('.new-com-cnt').show();
+            $('#name-com').focus();
+        });
+
+        /* when start writing the comment activate the "add" button */
+        $('.the-new-com').bind('input propertychange', function() {
+           $(".bt-add-com").css({opacity:0.6});
+           var checklength = $(this).val().length;
+           if(checklength){ $(".bt-add-com").css({opacity:1}); }
+        });
+
+        /* on clic  on the cancel button */
+        $('.bt-cancel-com').click(function(){
+            $('.the-new-com').val('');
+            $('.new-com-cnt').fadeOut('fast', function(){
+                $('.new-com-bt').fadeIn('fast');
+            });
+        });
+
+        // on post comment click 
+        $('.bt-add-com').click(function(){
+            var theCom = $('.the-new-com');
+            var theName = $('#name-com');
+            var theMail = $('#mail-com');
+
+            if( !theCom.val()){ 
+                alert('You need to write a comment!'); 
+            }else{ 
+                $.ajax({
+                    type: "POST",
+                    url: "ajax/add-comment.php",
+                    data: 'act=add-com&id_post='+<?php echo $id_post; ?>+'&name='+theName.val()+'&email='+theMail.val()+'&comment='+theCom.val(),
+                    success: function(html){
+                        theCom.val('');
+                        theMail.val('');
+                        theName.val('');
+                        $('.new-com-cnt').hide('fast', function(){
+                            $('.new-com-bt').show('fast');
+                            $('.new-com-bt').before(html);  
+                        })
+                    }  
+                });
+            }
+        });
+
+    });
+</script>
+
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="/dashboard/js/jquery.js"></script>
     <script src="/dashboard/js/bootstrap.min.js"></script>
