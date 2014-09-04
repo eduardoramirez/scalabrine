237feb84c9736fefceb7db3e5cfe7df71e5cc49a
Eduardo Ramirez <eduardo@ucsd.edu>
2014-09-04 13:52:45 -0700

    adding functionality to delete an error

--- dashboard/ajax/remove-comment.php
+++ dashboard/ajax/remove-comment.php
@@ -3,10 +3,10 @@ session_start();
 extract($_POST);
 if($_POST['act'] == 'rm-com')
 {
-
-    // Connect to the database
+  // Connect to the database
   include('../config2.php'); 
 
+  echo "WORKING";
   //insert the comment in the database
   mysql_query("DELETE FROM comments WHERE name='$name' and email='$email' and id_post='$id_post')");
 
