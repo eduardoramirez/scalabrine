4755aba6ef27d654e2ad0758ac3371ed25844e78
Alvin See <alvin.see@me.com>
2014-09-04 12:06:46 -0700

    add rating per comment

--- dashboard/error_detail.php
+++ dashboard/error_detail.php
@@ -232,24 +232,14 @@ include('config2.php');
         <div class="box-result-cnt">
             <?php
                 $query = mysql_query("SELECT * FROM ratings where id_post='$post_id'"); 
-                while($data = mysql_fetch_assoc($query)){
-                    $rate_db[] = $data;
-                    $sum_rates[] = $data['rate'];
-                }
-                if(@count($rate_db)){
-                    $rate_times = count($rate_db);
-                    $sum_rates = array_sum($sum_rates);
-                    $rate_value = $sum_rates/$rate_times;
-                    $rate_bg = (($rate_value)/5)*100;
-                }else{
-                    $rate_times = 0;
-                    $rate_value = 0;
-                    $rate_bg = 0;
-                }
+                $data = mysql_fetch_assoc($query);
+
+                  $rate_value = ($data['rate']/5)*100;
+
             ?>
 
             <div class="rate-result-cnt" id="<?php echo $post_id ?>">
-                <div class="rate-bg" style="width:<?php echo $rate_bg; ?>%"></div>
+                <div class="rate-bg" style="width:<?php echo $rate_value; ?>%"></div>
                 <div class="rate-stars"></div>
             </div>
 
