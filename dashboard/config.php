b13dbb51c2dad76d6564f4234b11dbc8abb872a3
Kenny Mai <kenmai9@gmail.com>
2014-09-03 17:56:24 -0700

    updated sidebar and header

--- dashboard/config.php
+++ dashboard/config.php
@@ -1,95 +1,171 @@
-<!DOCTYPE HTML>
-<html>
-   <head>
-      <title>scalabrine | team 16</title>
-      <meta charset="utf-8">
-      <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300italic,600%7CSource+Code+Pro" rel="stylesheet" />
-      <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
-      <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
-      <link rel="stylesheet" href="/css/other.css" />
-      <!--[if lte IE 8]><script src="js/html5shiv.js" type="text/javascript"></script><![endif]-->
-      <script src="/js/skel.min.js"></script>
-      <script>
-      skel.init({
-         prefix: '/css/style',
-         preloadStyleSheets: true,
-         resetCSS: true,
-         boxModel: 'border',
-         grid: { gutters: 30 },
-         breakpoints: {
-            wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
-            narrow: { range: '481-1199', containers: 960 },
-            mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
-         }
-      });
-      document.write('<link rel="stylesheet" href="/css/font-awesome.min.css" />')
-      </script>
-   </head>
+<?php
+   session_start();
+   if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
+      header("Location: /dashboard/login");
+   }
+   else
+   {
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="error tracking script">
+
+    <title>scalabrine | error detail</title>
+
+    <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+    <!-- Bootstrap core CSS -->
+    <link href="/dashboard/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/dashboard/css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="/dashboard/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+      <!-- Custom styles for this template -->
+    <link href="/dashboard/css/style.css" rel="stylesheet">
+    <link href="/dashboard/css/style-responsive.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
    <body>
-      <?php
-         session_start();
-         if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
-            header("Location: /login");
-         }
-      ?>
+      
       <div class="container">
-         <!-- Header -->
-         <header role="banner">
-            <div id="header" class="row">
-               <div class="4u">
-                  <div id="logo"></div>
-                  <h1>scalabrine</h1>
-               </div>
-               <div class="8u" id="nav">
-                  <a href="/config">config</a>
-                  <a href="/error_log">error logs</a>
-                  <a href="/gzip">gzip</a>
-                  <a href="/logout">logout</a>
-               </div>
-            </div>
-         </header>
-         <!-- Hero -->
-         <section id="hero">
-               <h2>configuration</h2>
-               <p>Include the appropriate JavaScript in every page of your web application.</p>
-         </section>
-         <!-- Main -->
-         <article>
-            <div>
-                <?php
+         <!--header start-->
+      <header class="header white-bg">
+         <div class="sidebar-toggle-box">
+            <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
+         </div>
+         <!--logo start-->
+         <a href="index.html" class="logo"><span>scalabrine</span></a>
+         <!--logo end-->
 
-                function getScriptName($name){
-                    return "http://scalabrine.net/track/" . $_SESSION['username'] 
-                        . "_" . $name . ".js";    
-                }
-                
-                ?>
+         <div class="nav notify-row" id="top_menu">
+            <!--  notification start -->
+            <a>
+               <i class="icon-bell-alt"></i>
+               last logged in: <?php echo $_SESSION['time'] ?>
+               <br>
+               <i class="icon-envelope"></i>
+               current user's email: <?php echo $_SESSION['email'] ?>
+            </a>
+         </div>
 
-            <table class="table table-bordered table-striped table-responsive">
-              <th>Application</th>
-              <th>Script</th>
-              <tr>
-                <td>app1</td>
-                <td><?php echo getScriptName("app1"); ?></td>
-              </tr>
-              <tr>
-                <td>app2</td>
-                <td><?php echo getScriptName("app2"); ?></td>
-              </tr>
-              <tr>
-                <td>app3</td>
-                <td><?php echo getScriptName("app3"); ?></td>
-              </tr>
-            </table>
-            </div>
-         </article>
-         <!-- Footer -->
-         <footer>
-            <div id="footer">
-               &copy; scalabrine.
-            </div>
-         </footer>
-      </div>
+         <div class="top-nav ">
+            <!--search & user info start-->
+            <ul class="nav pull-right top-menu">
+               <!-- user login dropdown start-->
+               <li class="dropdown">
+                  <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                     <img alt="" src="/dashboard/img/user.png" />
+                     <span class="username"><?php echo $_SESSION['username'] ?></span>
+                     <b class="caret"></b>
+                  </a>
+                  <ul class="dropdown-menu extended logout">
+                     <li class="log-arrow-up"></li>
+                     <li><a href="#"><i class="icon-cog"></i> Settings</a></li>
+                     <li><a href="/dashboard/logout"><i class="icon-key"></i> Log Out</a></li>
+                  </ul>
+               </li>
+               <!-- user login dropdown end -->
+            </ul>
+         </div>
+
+      </header>
+      <!--header end-->
+
+      <!--sidebar start-->
+      <aside>
+         <div id="sidebar"  class="nav-collapse ">
+            <!-- sidebar menu start-->
+            <ul class="sidebar-menu" id="nav-accordion">
+               <li>
+                  <a href="index">
+                     <i class="icon-dashboard"></i>
+                     <span>dashboard</span>
+                  </a>
+               </li>
+
+               <li>
+                  <a class="active" href="error_detail">
+                     <i class="icon-tasks"></i>
+                     <span>error details</span>
+                  </a>
+               </li>
+
+               <li>
+                  <a href="/crud/index">
+                     <i class="icon-gear"></i>
+                     <span>crud</span>
+                  </a>
+               </li>
+               <li>
+                  <a href="/jserrorreporter">
+                     <i class="icon-exclamation"></i>
+                     <span>error reporter</span>
+                  </a>
+               </li>
+               <li>
+                  <a href="/index">
+                     <i class="icon-user"></i>
+                     <span>home</span>
+                  </a>
+               </li>
+
+            </ul>
+            <!-- sidebar menu end-->
+         </div>
+      </aside>
+      <!--sidebar end-->
+      
+      <!-- Hero -->
+      <section id="hero">
+            <h2>configuration</h2>
+            <p>Include the appropriate JavaScript in every page of your web application.</p>
+      </section>
+      <!-- Main -->
+      <article>
+         <div>
+             <?php
+
+             function getScriptName($name){
+                 return "http://scalabrine.net/track/" . $_SESSION['username'] 
+                     . "_" . $name . ".js";    
+             }
+             
+             ?>
+
+         <table class="table table-bordered table-striped table-responsive">
+           <th>Application</th>
+           <th>Script</th>
+           <tr>
+             <td>app1</td>
+             <td><?php echo getScriptName("app1"); ?></td>
+           </tr>
+           <tr>
+             <td>app2</td>
+             <td><?php echo getScriptName("app2"); ?></td>
+           </tr>
+           <tr>
+             <td>app3</td>
+             <td><?php echo getScriptName("app3"); ?></td>
+           </tr>
+         </table>
+         </div>
+      </article>
+      <!-- Footer -->
+      <footer>
+         <div id="footer">
+            &copy; scalabrine.
+         </div>
+      </footer>
+   </div>
 
    <!-- Piwik -->
    <script type="text/javascript">
