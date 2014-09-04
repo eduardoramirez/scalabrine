7b229db11e1657baa23f1d94fb83fc37336bdc3c
Alvin See <alvin.see@me.com>
2014-09-04 11:44:35 -0700

    add rating per comment

--- rating/ajax.php
+++ rating/ajax.php
@@ -7,15 +7,15 @@ require_once 'config.php';
     	$therate = $_POST['rate'];
     	$thepost = $_POST['post_id'];
 
-    	$query = mysql_query("SELECT * FROM ratings where ip= '$ip'  "); 
-    	while($data = mysql_fetch_assoc($query)){
-    		$rate_db[] = $data;
-    	}
+    	//$query = mysql_query("SELECT * FROM ratings where ip= '$ip'  "); 
+    	//while($data = mysql_fetch_assoc($query)){
+    	//	$rate_db[] = $data;
+    	//}
 
-    	if(@count($rate_db) == 0 ){
+    	//if(@count($rate_db) == 0 ){
     		mysql_query("INSERT INTO ratings (id_post, ip, rate)VALUES('$thepost', '$ip', '$therate')");
-    	}else{
-    		mysql_query("UPDATE ratings SET rate= '$therate' WHERE ip = '$ip'");
-    	}
+    	//}else{
+    	//	mysql_query("UPDATE ratings SET rate= '$therate' WHERE ip = '$ip'");
+    	//}
     } 
 ?>
