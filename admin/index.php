75c72683843f2a2963320bf210bd8649334d25d8
Kenny Mai <kenmai9@gmail.com>
2014-09-03 23:00:55 -0700

    changed page title

--- admin/index.php
+++ admin/index.php
@@ -17,7 +17,7 @@ else{
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="description" content="dashboard">
 
-      <title>scalabrine | crud</title>
+      <title>scalabrine | admin clients</title>
 
       <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
       <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
