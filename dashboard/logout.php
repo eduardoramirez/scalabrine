2bfaee58ee97502462eb36ead8ae07f050584795
Matt Asaro <mattasaro@gmail.com>
2014-09-03 21:53:29 -0700

    fixed log out event for session log

--- dashboard/logout.php
+++ dashboard/logout.php
@@ -1,17 +1,21 @@
 <?php
   session_start();
 
-    if(isset($_SESSION['username'])){
-        $username = $_SESSION['username'];
-        $data = my_query('s', array(&$username), "SELECT id FROM user WHERE Username = ?");
-        $id = $data['id'];
+if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
+    header("Location: /dashboard/login");
+}
+else{
 
-        require '../userLog.php';
-        recordEvent('log out', $id);
-    }
+    $username = $_SESSION['username'];
+    $data = my_query('s', array(&$username), "SELECT id FROM user WHERE Username = ?");
+    $id = $data['id'];
+
+    require '../userLog.php';
+    recordEvent('log out', $id);
 
   if(session_destroy())
   {
     header("Location: /dashboard/login");
   }
-?>
+
+}
