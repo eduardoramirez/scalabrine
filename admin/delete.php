6be4047b972cbe57b668168a49c5d07f6460a7f6
Matt Asaro <mattasaro@gmail.com>
2014-09-03 19:14:51 -0700

    added admin create and delete pages

---
+++ admin/delete.php
@@ -0,0 +1,204 @@
+<?php
+session_start();
+
+if ( !empty($_GET['id'])) {
+    $id = $_REQUEST['id'];
+}
+
+if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
+    header("Location: /dashboard/login");
+}
+else if ($_SESSION['admin'] != 2){
+    header("HTTP/1.1 403 Forbidden");
+    header("Location: /403");
+}
+else{
+	require '../database.php';
+
+  $id = 0;
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+
+
+    if ( !empty($_POST)) {
+        // keep track post values
+        $id = $_POST['id'];
+
+        $sql = "delete from user where orgID = ?; DELETE FROM organization WHERE ID = ?";
+        my_update('ii', array(&$id, &$id), $sql);
+
+        my_disconnect();
+        header("Location: index");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="dashboard">
+
+      <title>scalabrine | delete</title>
+
+      <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
+      <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+      <!-- Bootstrap core CSS -->
+      <link href="/crud/css/bootstrap.min.css" rel="stylesheet">
+      <link href="/dashboard/css/bootstrap-reset.css" rel="stylesheet">
+      <!--external css-->
+      <link href="/dashboard/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+      <!-- Custom styles for this template -->
+      <link href="/dashboard/assets/advanced-datatable/media/css/demo_table.css" rel="stylesheet" />    
+      <link href="/dashboard/css/style.css" rel="stylesheet">
+      <link href="/dashboard/css/style-responsive.css" rel="stylesheet" />
+
+
+      <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+      <!--[if lt IE 9]>
+         <script src="/login/js/html5shiv.js"></script>
+         <script src="/login/js/respond.min.js"></script>
+      <![endif]-->  
+</head>
+
+<body>
+
+   <div id="container" >
+
+      <!--header start-->
+      <header class="header white-bg">
+         <div class="sidebar-toggle-box">
+            <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
+         </div>
+         <!--logo start-->
+         <a href="/dashboard/index" class="logo"><span>scalabrine</span></a>
+         <!--logo end-->
+
+         <div class="nav notify-row" id="top_menu">
+            <!--  notification start -->
+            <a>
+               <i class="icon-bell-alt"></i>
+               last logged in: <?php echo $_SESSION['time'] ?>
+               <br>
+               <i class="icon-envelope"></i>
+               current user's email: <?php echo $_SESSION['email'] ?>
+            </a>
+         </div>
+
+         <div class="top-nav ">
+            <!--user start-->
+            
+            <ul class="nav pull-right top-menu">
+               
+               <!-- user login dropdown start-->
+               <li class="dropdown">
+                  <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                     <img alt="" src="/dashboard/img/user.png" />
+                     <span class="username"><?php echo $_SESSION['username'] ?></span>
+                     <b class="caret"></b>
+                  </a>
+                  <ul class="dropdown-menu extended logout">
+                     <li class="log-arrow-up"></li>
+                     <li><a href="#"><i class="icon-cog"></i> Settings</a></li>
+                     <li><a href="/dashboard/logout"><i class="icon-key"></i> Log Out</a></li>
+                  </ul>
+               </li>
+               <!-- user login dropdown end -->
+            </ul>
+         </div>
+
+      </header>
+      <!--header end-->
+
+      <!--sidebar start-->
+      <aside>
+         <div id="sidebar"  class="nav-collapse ">
+            <!-- sidebar menu start-->
+             <ul class="sidebar-menu" id="nav-accordion">
+                 <li>
+                     <a href="/admin/index">
+                         <i class="icon-tasks"></i>
+                         <span>clients</span>
+                     </a>
+                 </li>
+
+                 <li>
+                     <a href="/dashboard/users">
+                         <i class="icon-gear"></i>
+                         <span>users</span>
+                     </a>
+                 </li>
+
+                 <li>
+                     <a class="active" href="/admin/audit">
+                         <i class="icon-exclamation"></i>
+                         <span>audit log</span>
+                     </a>
+                 </li>
+                 <li>
+                     <a href="/admin/stats">
+                         <i class="icon-file"></i>
+                         <span>stats</span>
+                     </a>
+                 </li>
+             </ul>
+            <!-- sidebar menu end-->
+         </div>
+      </aside>
+      <!--sidebar end-->
+
+      <!--main content start-->
+      <div id="main-content">
+         <div class="wrapper">
+    
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Delete a User</h3>
+		    		</div>
+		    		
+	    			<form class="form-horizontal" action="delete" method="post">
+	    			  <input type="hidden" name="id" value="<?php echo $id;?>"/>
+					  <p class="alert alert-error">This will also delete all users associated with
+                          this organization. Are you sure you want to delete?</p>
+					  <div class="form-actions">
+						  <button type="submit" class="btn btn-danger">Yes</button>
+						  <a class="btn" href="index">No</a>
+						</div>
+					</form>
+				</div>
+				
+        </div> 
+        </div>
+        <!-- /container -->
+          <!--main content end-->
+      <!--footer start-->
+      <footer class="site-footer">
+          <div class="text-center">
+              2014 &copy; scalabrine.
+              <a href="#" class="go-top">
+                  <i class="icon-angle-up"></i>
+              </a>
+          </div>
+      </footer>
+      <!--footer end-->
+
+   <!-- js placed at the end of the document so the pages load faster -->
+   <script src="/dashboard/js/jquery-1.8.3.min.js"></script>
+   <script src="/dashboard/js/bootstrap.min.js"></script>
+   <script src="/dashboard/js/jquery.dcjqaccordion.2.7.js"></script>
+   <script src="/dashboard/js/jquery.scrollTo.min.js"></script>
+   <script src="/dashboard/js/jquery.nicescroll.js"></script>
+   <script src="/dashboard/js/jquery.sparkline.js"></script>
+   <script src="/dashboard/assets/advanced-datatable/media/js/jquery.dataTables.js"></script>
+   <script src="/dashboard/js/jquery.customSelect.min.js" ></script>
+   <script src="/dashboard/js/respond.min.js" ></script>
+
+   <!--common script for all pages-->
+   <script src="/dashboard/js/common-scripts.js"></script>      
+  </body>
+</html>
+<?php
+}
+?>
