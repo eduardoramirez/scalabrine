d6c562eb0dc4f0131bb5d0e87fe3bdfbf9ef787f
Matt Asaro <mattasaro@gmail.com>
2014-09-03 21:46:27 -0700

    added log out event to session log

--- dashboard/logout.php
+++ dashboard/logout.php
@@ -1,5 +1,14 @@
 <?php
   session_start();
+
+    if(isset($_SESSION['username'])){
+        $data = my_query('s', array(&$username), "SELECT ID FROM user WHERE Username=?");
+        $id = $data['ID'];
+
+        require '../database.php';
+        recordEvent('log out', $id);
+    }
+
   if(session_destroy())
   {
     header("Location: /dashboard/login");
