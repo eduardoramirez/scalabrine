304e2bce8092bbffa68e39078a19e52696b0304d
Alvin See <alvin.see@me.com>
2014-09-04 21:50:17 -0700

    orgid incremented per user creation

--- dashboard/registration.php
+++ dashboard/registration.php
@@ -35,8 +35,8 @@ else
         $roleAdmin = "1";
         $con  = new mysqli("localhost","root","Tw0sof+9Ly","scalabrinedb");
         $stuff = $con->query("SELECT MAX(OrgID) AS orgid FROM user");
-        echo $stuff;
-        $orgid = "1";
+
+        $orgid = stuff['orgid'];
 
         my_update('sssss', array(&$username, &$email, &$h_password, &$roleAdmin, &$orgid), $sql);
         $stuff->close();
