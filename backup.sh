b60f5c9e73a4ae095c396cf8a548514eea52c4ab
Eduardo Ramirez <eduardo@ucsd.edu>
2014-09-05 10:54:25 -0700

    fixed path issues with backup

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to backup directories
-DIRS="/var/www/html/ /etc"
+DIRS="/var/www/html/  x/etc"
  
 # Store todays date
 NOW=$(date +"%F")
@@ -12,14 +12,10 @@ BACKUP="/backup/$NOW"
 # Backup file name hostname.time.tar.gz 
 BFILE="$(hostname).$(date +'%T').tar.gz"
 MFILE="$(hostname).$(date +'%T').mysql.sq.gz"
-
  
 # Set MySQL username and password
 MYSQLUSER="root"
 MYSQLPASSWORD="Tw0sof+9Ly"
- 
-# Location to store backup
-DUMPDIR="/backup/"
 
 # Paths for binary files
 TAR="/bin/tar"
@@ -41,9 +37,6 @@ $TAR -zcvf ${BACKUP}/${BFILE} "${DIRS}"
 # Backup MySQL
 $MYSQLDUMP  -u ${MYSQLUSER} -h localhost -p${MYSQLPASSWORD} --all-databases | $GZIP -9 > ${BACKUP}/${MFILE}
  
-# Copy things to directory saved
-$cp -r ${BACKUP}/* ${DUMPDIR}/${NOW} 
-
 # Log backup end time in /var/log/messages
 $LOGGER "$0: *** Backup Ended @ $(date) ***"
 
