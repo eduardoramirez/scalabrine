cc6b09f12255191b7bd2450133f7320db831e493
Eduardo Ramirez <eduardo@ucsd.edu>
2014-09-05 13:54:28 -0700

    making add-comment follow unified db

--- dashboard/error_detail.php
+++ dashboard/error_detail.php
@@ -110,7 +110,7 @@
                <li>
                   <a href="/crud/index">
                      <i class="icon-gear"></i>
-                     <span>User Management</span>
+                     <span>user management</span>
                   </a>
                </li>
                <li>
