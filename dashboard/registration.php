a4387db295bc7b9379347b07203c0b7790075799
Alvin See <alvin.see@me.com>
2014-09-04 21:10:11 -0700

    orgid default to 1

--- dashboard/registration.php
+++ dashboard/registration.php
@@ -31,8 +31,12 @@ else
       {
         $h_password = password_hash($password, PASSWORD_BCRYPT, $options);
         $sql="INSERT INTO user (username, email, password, admin, OrgID) VALUES (?, ?, ?, ?, ?)";
+        $norgid="SELECT MAX(OrgID) AS orgid FROM user";
         $roleAdmin = "1";
-        $orgid = "1";
+        
+        $data = my_query('s', array(&$username), $norgid);
+
+        $orgid = $data['OrgID'] + 1;
 
         my_update('sssss', array(&$username, &$email, &$h_password, &$roleAdmin, &$orgid), $sql);
 
