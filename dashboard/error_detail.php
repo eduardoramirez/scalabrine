ac1f7eb34179c0056bc67936667a7d7afbc08bc4
Eduardo Ramirez <eduardo@ucsd.edu>
2014-09-05 18:38:43 -0700

    fixed db problems

--- dashboard/error_detail.php
+++ dashboard/error_detail.php
@@ -28,6 +28,11 @@
     <link href="/dashboard/css/style-responsive.css" rel="stylesheet" />
 
 
+    <link type="text/css" rel="stylesheet" href="/comments/css/style.css">
+    <link type="text/css" rel="stylesheet" href="/comments/css/example.css">
+
+    <link type="text/css" rel="stylesheet" href="/rating/css/style.css">
+    <link type="text/css" rel="stylesheet" href="/rating/css/example.css">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
     <!--[if lt IE 9]>
