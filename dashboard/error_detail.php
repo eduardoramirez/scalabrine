35fba73325dcc29b646404e0eb35e91d95844a1d
Eduardo Ramirez <eduardo@ucsd.edu>
2014-09-05 18:52:47 -0700

    fixing css styling issues in error_page

--- dashboard/error_detail.php
+++ dashboard/error_detail.php
@@ -32,7 +32,6 @@
     <link type="text/css" rel="stylesheet" href="/comments/css/example.css">
 
     <link type="text/css" rel="stylesheet" href="/rating/css/style.css">
-    <link type="text/css" rel="stylesheet" href="/rating/css/example.css">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
     <!--[if lt IE 9]>
